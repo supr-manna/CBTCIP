0325dce2ff2fed591055b644f6982bc9bba73eb1
Supriya Manna <80158174+supr-manna@users.noreply.github.com>
2024-06-09 21:31:09 +0530

    App File

---
+++ QuizApp/app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.gmsService)
+}
+
+android {
+    namespace = "com.supriya.quizapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.supriya.quizapp"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    viewBinding {
+        enable = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.firebase.firestore)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation(platform(libs.firebaseBom))
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.gms)
+    implementation(libs.glide)
+}
